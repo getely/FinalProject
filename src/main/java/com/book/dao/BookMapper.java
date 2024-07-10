bd9d40a5e24d1e6ef92fce14d9c23e1a91409c93
getely <2894516794@qq.com>
2024-07-10 20:53:38 +0800

    Update BookMapper.java

--- src/main/java/com/book/dao/BookMapper.java
+++ src/main/java/com/book/dao/BookMapper.java
@@ -25,5 +25,6 @@ public interface BookMapper {
     @Select("select * from book")
     List<Book> getBookList();
 
-
+    @Insert("insert into book(title,`desc`,price) values(#{title},#{desc},#{price})")//插入书本名，简介和价格
+    void InsertBook(@Param("title") String title,@Param("desc") String desc,@Param("price") double price);
 }
